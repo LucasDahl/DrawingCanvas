fdf68d0dd8fc930250b985a6a65cd0f7187f4fa6
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-17 07:07:50 -0800

    Added the ability to draw lines(always connects the last point).

--- DrawingCanvas/ViewController.swift
+++ DrawingCanvas/ViewController.swift
@@ -10,10 +10,61 @@ import UIKit
 
 class Canvas: UIView {
     
+    // Properties
+    var line = [CGPoint]()
+    
     override func draw(_ rect: CGRect) {
         
         // Custom drawing
+        super.draw(rect)
         
+        // Get a hold of the context
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        // Setup the lines
+        
+        // Test dummy data
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 100, y: 100)
+//        context.move(to: startPoint)
+//        context.addLine(to: endPoint)
+        
+        // Iterate through the array
+        for (i, p) in line.enumerated() {
+            
+            if i == 0 {
+                
+                // Begin the line
+                context.move(to: p)
+                
+            } else {
+                
+                // Continue the line
+                context.addLine(to: p)
+                
+            }
+            
+        }
+        
+        // Helps paint a line on the canvas
+        context.strokePath()
+        
+        
+        
+    }
+    
+    // Track the users finger - Captures where the finger moved
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        // Get the first point touched
+        guard let point = touches.first?.location(in: nil) else { return }
+        //print(point)
+        
+        // Apend the array woth the touched points
+        line.append(point)
+        
+        // Redraw the line
+        setNeedsDisplay()
         
     }
     
@@ -29,6 +80,9 @@ class ViewController: UIViewController {
         
         // Add canvas to the subview
         view.addSubview(canvas)
+        
+        // Set the background color
+        canvas.backgroundColor = UIColor.white
         
         // Set the frame
         canvas.frame = view.frame
