a4c5d86731ef565dc1b0a6e60654ba975f3f5a0a
Lucas Dahl <LucasDahl425@yahoo.com>
2019-01-18 06:46:38 -0800

    Added the clear and undo buttons.

--- DrawingCanvas/Controller/ViewController.swift
+++ DrawingCanvas/Controller/ViewController.swift
@@ -10,21 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    // Properties
+    //====================
+    // MARK: - Properties
+    //====================
+    
     let canvas = Canvas()
+    
+    
+    //================
+    // MARK: - Buttons
+    //================
+    
+    let undoButton: UIButton = {
+        
+        let button = UIButton(type: .system)
+        button.setTitle("Undo", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return button
+        
+    }()
+    
+    let clearBUtton: UIButton = {
+       
+        let button = UIButton(type: .system)
+        button.setTitle("Clear", for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return button
+        
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Add canvas to the subview
-        view.addSubview(canvas)
-        
         // Set the background color
         canvas.backgroundColor = UIColor.white
         
-        // Set the frame
-        canvas.frame = view.frame
+        // Setup the stackView
+        let stackView = UIStackView(arrangedSubviews: [undoButton, clearBUtton])
         
+        // Add the stackview to the subview
+        view.addSubview(stackView)
+        
+        
+    }
+    
+    // Use this to set the frame and add the canvas to the subview
+    override func loadView() {
+        self.view = canvas
     }
 
 
--- DrawingCanvas/Views/Canvas.swift
+++ DrawingCanvas/Views/Canvas.swift
@@ -84,4 +84,4 @@ class Canvas: UIView {
         
     }
     
-} // End class
+}
